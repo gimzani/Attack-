76489761c0048bd2b489c5020a86c97844576764
TheStormKing <greg.harter@gmail.com>
2017-03-11 13:22:45 -0500

    Add files via upload

--- services/newPlayer.php
+++ services/newPlayer.php
@@ -11,12 +11,12 @@ $e = $_GET["e"];
 $lt = $_GET["lt"];
 $lg = $_GET["lg"];
 
-$rgb = rand_color();
+$tn = rand_color($cxn);
 
-$query = "INSERT INTO Player (email, PlayerName, Team, Nick, Latitude, Longitude)
+$query = "INSERT INTO Player (email, PlayerName, TeamName, Nick, Latitude, Longitude)
 				SELECT '$e'
 				, '$pn'
-				, '$rgb'
+				, '$tn'
 				, '$n'
 				, $lt
 				, $lg;";
@@ -25,7 +25,7 @@ $rs = mysqli_query($cxn,$query) or die(mysqli_error($cxn));
 
 $id = mysqli_insert_id($cxn);
 
-$query = "SELECT Player_ID, Team FROM Player
+$query = "SELECT Player_ID, TeamName FROM Player
 				WHERE Player_ID = $id;";
 
 $rs=mysqli_query($cxn,$query) or die(mysqli_error($cxn));
@@ -42,8 +42,14 @@ while($row = mysqli_fetch_assoc($rs)) {
 }
 print json_encode($rows);
 
-function rand_color() {
-    return sprintf('#%06X', mt_rand(0, 0xFFFFFF));
+function rand_color($cxn) {
+	$query = "SELECT TeamName FROM Team WHERE IsActive = 1 ORDER BY RAND() LIMIT 1;";
+	$rs=mysqli_query($cxn,$query) or die(mysqli_error($cxn));
+	while($row = mysqli_fetch_assoc($rs)) {
+		return $row["TeamName"];
+	}
+	
+    //return sprintf('#%06X', mt_rand(0, 0xFFFFFF));
 }
 
 ?>
